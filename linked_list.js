fe7d0a5378d02b8d2c08c468f977d2711ce862e8
tusbea <tusbea@gmail.com>
2016-08-19 10:04:39 +0900

    일단 동작은 함

--- linked_list.js
+++ linked_list.js
@@ -5,12 +5,16 @@ var margin = {top: 40, right: 20, bottom: 30, left: 40},
 var data = [
 	[1],
 	[1, 2],
-	[1, 2, 3],
-	[1, 2, 4, 3]
+	[3, 1, 2],
+	[3, 1, 4, 2],
+	[3, 1, 5, 4, 2],
+	[6, 3, 1, 5, 4, 2],
+	[6, 3, 1, 5, 4, 2, 7],
+	[6, 3, 1, 8, 5, 4, 2, 7]
 ];
-var idx = 0;
+var idx = 7;
 var prev_data, cur_data;
-var delay = 1000, duration = 1000;
+var delay = 1000, duration = 500;
 
 var svg = d3.select(".canvas").append("svg")
     .attr("width", width + margin.left + margin.right)
@@ -40,14 +44,14 @@ redraw(cur_data);
     transition 
         .each(function() { ++n; }) 
         .each("end", function() { if (!--n) callback.apply(this); }); 
-  } 
+} 
 
-var inc = 1;  
+var inc = 0;  
 
 function step() {
 	if (inc) {
 		idx++;
-		if (idx === 3)
+		if (idx === 7)
 			inc = 0;
 	}
 	else {
@@ -106,7 +110,6 @@ function add_node(idx, val, cd)
 	var arrows = svg.selectAll(".arrow");
 	
 	if (cd.length - 1 === idx) {	// 맨 뒤. arrow, node 추가
-		console.log("nodes : " + nodes);
 		nodes = nodes.data(cd)
 			.enter().append("rect")
 			.attr("class", "node")
@@ -144,50 +147,56 @@ function add_node(idx, val, cd)
 			.style("opacity", 1);
 	}
 	else {	// node, arrow 추가
-		
+		nodes.transition().duration(duration)
+			.attr("x", function(d, i) { return i >= idx ? (i+1) * 80 : i * 80; });
+		texts.transition().duration(duration)
+			.attr("x", function(d, i) { return i >= idx ? (i+1) * 80 + 15 : i * 80 + 15; });
+		arrows.transition().duration(duration)
+			.attr("transform", function(d, i) { return "translate(" + (i >= idx ? (i+1) * 80 : i * 80) + ",15)";})
+			.call(endall, function() {
+				nodes = nodes.data(cd);
+				nodes.enter().append("rect")
+					.attr("class", "node")
+					.attr("x", function(d, i) { return i * 80; })
+					.attr("y", 0)
+					.attr("width", 30)
+					.attr("height", 30)
+					.style("opacity", function(d, i) { return i === idx ? 0 : 1; });
+				nodes.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("x", function(d, i) { return i * 80; });
+				
+				texts = texts.data(cd);
+				texts.enter().append("text")
+					.attr("class", "text")
+					.attr("x", function(d, i) { return i * 80 + 15; })
+					.attr("y", 20)
+					.attr("text-anchor", "middle")
+					.text(function(d) { return d; })
+					.style("opacity", function(d, i) { return i === idx ? 0 : 1; });
+				texts.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("x", function(d, i) { return i * 80 + 15; })
+					.text(function(d) { return d; });
+				
+				arrows = arrows.data(cd.slice(0, -1));
+				arrows.enter().append("path")
+					.attr("class", "arrow")
+					.attr("transform", function(d, i) { return "translate(" + (i * 80) + ",15)" })
+					.attr("d", "M40,0 L70,0")
+					.style("marker-end", "url(#arrow)")
+					.style("stroke", "black")
+					.style("stroke-width", "3px")
+					.style("opacity", function(d, i) { return i === idx ? 0 : 1; });
+				arrows.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("transform", function(d, i) { return "translate(" + (i * 80) + ",15)" });
+					
+				nodes.transition().duration(duration)
+					.style("opacity", 1);
+				texts.transition().duration(duration)
+					.style("opacity", 1);
+				arrows.transition().duration(duration)
+					.style("opacity", 1);
+			});
 	}
-	
-	// 1단계 : 추가될 곳의 오른쪽에 있는 node들을 한 칸씩 밀기
-	//nodes.transition().duration(duration)
-	//	.attr("x", function(d, i) { 
-	//		if (i >= idx) 	return (i + 1) * 80;
-	//		else			return i * 80;
-	//	});
-	//	
-	//texts.transition().duration(duration)
-	//	.attr("x", function(d, i) { 
-	//		if (i >= idx) 	return (i + 1) * 80 + 15; 
-	//		else 			return i * 80 + 15;
-	//	});
-	//	
-	//arrows.transition().duration(duration)
-	//	.attr("transform", function(d, i) { 
-	//		if (i >= idx - 1) 	return "translate(" + ((i+1) * 80) + ",15)";
-	//		else			return "translate(" + (i * 80) + ",15)";
-	//	});
-	//	
-	//svg.data([val]).enter().append("rect")
-	//	.attr("class", "node")
-	//	.attr("x", function(d, i) { return idx * 80; })
-	//	.attr("y", 0)
-	//	.attr("width", 30)
-	//	.attr("height", 30);
-	//
-	//// 2단계 : node와 arrow 추가
-	//nodes.filter(function(d, i) { return i === idx; })
-	//	.transition().duration(duration).delay(delay)
-	//	.style("opacity", 1);
-	//	
-	//texts.filter(function(d, i) { return i === idx; })
-	//	.transition().duration(duration).delay(delay)
-	//	.style("opacity", 1);
-	//	
-	//arrows.filter(function(d, i) { return i === idx - 1; })
-	//	.transition().duration(duration).delay(delay)
-	//	.style("opacity", 1);
-	
-	// 3단계 : redraw
-	
 }
 
 function delete_node(idx, cd)
@@ -199,85 +208,77 @@ function delete_node(idx, cd)
 	var arrows = svg.selectAll(".arrow");
 	
 	if (cd.length === idx) {	// 맨 뒤. arrow, node 삭제
-		
+		nodes.filter(function(d, i) { return i === idx; })
+			.transition().duration(duration)
+			.style("opacity", 0);
+			
+		texts.filter(function(d, i) { return i === idx; })
+			.transition().duration(duration)
+			.style("opacity", 0);
+			
+		arrows.filter(function(d, i) { return i === idx - 1; })
+			.transition().duration(duration)
+			.style("opacity", 0)
+			.call(endall, function() {
+				nodes = nodes.data(cd);
+				texts = texts.data(cd);
+				arrows = arrows.data(cd.slice(0, -1));
+				
+				nodes.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("x", function(d, i) { return i * 80; });
+				texts.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("x", function(d, i) { return i * 80 + 15; })
+					.text(function(d) { return d; });
+				arrows.style("opacity", function(d, i) { return i === idx ? 0 : 1; })
+					.attr("transform", function(d, i) { return "translate(" + (i * 80) + ",15)" });
+							
+				
+				nodes.exit().remove();
+				texts.exit().remove();
+				arrows.exit().remove();
+			});
 	}
 	else {	// node, arrow 삭제
-		
+		nodes.filter(function(d, i) { return i === idx; })
+			.transition().duration(duration)
+			.style("opacity", 0);
+			
+		texts.filter(function(d, i) { return i === idx; })
+			.transition().duration(duration)
+			.style("opacity", 0);
+			
+		arrows.filter(function(d, i) { return i === idx; })
+			.transition().duration(duration)
+			.style("opacity", 0)
+			.call(endall, function() {
+				nodes.transition().duration(duration)
+					.attr("x", function(d, i) { return i > idx ? (i-1) * 80 : i * 80; });
+				texts.transition().duration(duration)
+					.attr("x", function(d, i) { return i > idx ? (i-1) * 80 + 15 : i * 80 + 15; });
+				arrows.transition().duration(duration)
+					.attr("transform", function(d, i) { return "translate(" + (i > idx ? (i-1) * 80 : i * 80) + ",15)" })
+					.call(endall, function() {
+						nodes = nodes.data(cd);
+						texts = texts.data(cd);
+						arrows = arrows.data(cd.slice(0, -1));
+						
+						nodes.style("opacity", 1)
+							.attr("x", function(d, i) { return i * 80; });
+						texts.style("opacity", 1)
+							.attr("x", function(d, i) { return i * 80 + 15; })
+							.text(function(d) { return d; });
+						arrows.style("opacity", 1)
+							.attr("transform", function(d, i) { return "translate(" + (i * 80) + ",15)" });
+									
+						
+						nodes.exit().remove();
+						texts.exit().remove();
+						arrows.exit().remove();
+					});
+			});
 	}
 	
-	// 1단계 : node와 arrow 사라짐
-	nodes.filter(function(d, i) { return i === idx; })
-		.transition().duration(duration)
-		.style("opacity", 0);
-		
-	texts.filter(function(d, i) { return i === idx; })
-		.transition().duration(duration)
-		.style("opacity", 0);
-		
-	arrows.filter(function(d, i) { return i === idx - 1; })
-		.transition().duration(duration)
-		.style("opacity", 0);
-		
-	// 2단계 : 오른쪽에 있는 node가 왼쪽으로 붙음
-	nodes.transition().delay(delay).duration(duration)
-		.attr("x", function(d, i) { 
-			if (i > idx) 	return (i - 1) * 80;
-			else			return i * 80;
-		});
-		
-	texts.transition().delay(delay).duration(duration)
-		.attr("x", function(d, i) { 
-			if (i > idx) 	return (i - 1) * 80 + 15; 
-			else 			return i * 80 + 15;
-		});
-		
-	arrows.transition().delay(delay).duration(duration)
-		.attr("transform", function(d, i) { 
-			if (i >= idx) 	return "translate(" + ((i-1) * 80) + ",15)";
-			else			return "translate(" + (i * 80) + ",15)";
-		})
-		.call(endall, function() { redraw(cd); });
 	
-	
-	// 3단계 : data 재설정(필요 없나?)
-	//redraw(cd);
-	
-	
-	
-	
-	//nodes.data(d).enter().append("rect")
-	//	.attr("class", "node")
-	//	.attr("x", function(d, i) { return i * 80; })
-	//	.attr("y", 0)
-	//	.attr("width", 30)
-	//	.attr("height", 30);
-	//nodes.data(d).exit().remove();
-	//	
-	//	
-	//texts.data(d).enter().append("text")
-	//	.attr("class", "text")
-	//	.attr("x", function(d, i) { return i * 80 + 15; })
-	//	.attr("y", 20)
-	//	.attr("text-anchor", "middle")
-	//	.text(function(d) { return d; });
-	//texts
-	//	.text(function(d) { return d; });
-	//texts.data(d).exit().remove();
-	//	
-	//	
-	//arrows.enter().append("path")
-	//	.attr("class", "arrow")
-	//	.attr("x", function(d, i) { return i * 80 + 40; })
-	//	.attr("y", 0)
-	//	.attr("d", function(d, i) {
-	//		var x1 = 40;
-	//		var x2 = 70;
-	//		return "M" + x1 + ",15 L" + x2 + ",15";
-	//	})
-	//	.style("marker-end", "url(#arrow)")
-	//	.style("stroke", "black")
-	//	.style("stroke-width", "3px");
-	//arrows.exit().remove();
 }
 
 function change_data(pd, cd)
